a7ea73a739726240070ea1da9898e6155752f1e7
Daniel Moris <inge.moris@gmail.com>
2023-10-28 18:28:09 -0300

    Version con comentarios en la clase definida en la Capa de Datos

--- Datos/ConexionSQL.cs
+++ Datos/ConexionSQL.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+/********************************************************************* */
+/*  Debemos agregar estos dos "using" para poder manejar las tablas de */
+/*  la Base de Datos                                                   */
+/***********************************************************************/
 using System.Data.SqlClient;
 using System.Threading;
 
@@ -11,21 +15,27 @@ namespace Datos
 {
     public class ConexionSQL
     {
+        /* Conexion a la Base de Datos */
         static string ConexionStr = "server= NEWMORIS\\SQLEXPRESS ; database= PV;" +
             "integrated security=true";
         SqlConnection con= new SqlConnection(ConexionStr);
+        /*******************************/
 
         public int consultaLogin(string username, string password) 
         {
-            con.Open();
+            con.Open(); /* Abre la Base de Datos*/
             int contador;
+
+            /* Creo la cadena de caracteres de los comandos de consulta de SQL */
             string stringQuery = "select count(*) from Personal where usuario='"+username+"'" +
                 " and clave='"+password+"'";
 
+
+            /* Hace la consulta */
             SqlCommand conexion = new SqlCommand(stringQuery,con);
             contador = Convert.ToInt32(conexion.ExecuteScalar());
             
-            con.Close();
+            con.Close(); /* Cierra la base de Datos */
             return contador;
         }
 
